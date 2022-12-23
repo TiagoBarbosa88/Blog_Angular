89e79c9dc7eb10de6058e20a0d40cbb083215a35
TiagoBarbosa88 <sombr43@gmail.com>
2022-12-23 08:07:31 -0300

    New Post Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { NewPostFormComponent } from './components/template/new-post-form/new-post-form.component';
-import {HttpClientModule } from '@angular/common/http';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NewPostCardComponent } from './components/template/new-post-card/new-post-card.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NewPostFormComponent
+    NewPostFormComponent,
+    NewPostCardComponent
+
   ],
   imports: [
     BrowserModule,
@@ -25,7 +31,9 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatMenuModule,
     MatButtonModule,
     HttpClientModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatCardModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/template/new-post-card/new-post-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-new-post-card',
+  templateUrl: './new-post-card.component.html',
+  styleUrls: ['./new-post-card.component.scss']
+})
+export class NewPostCardComponent implements OnInit {
+
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+
+
+  newPost(){
+    this.dialog.open(NewPostCardComponent)
+
+  }
+}
+
